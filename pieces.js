ae934e4e9da24bea5283271cc9e8ac5f29b76c08
Roxane <roxane.vidoni@gmail.com>
2024-04-08 22:48:30 +0200

    Fiche and articles created. CSS updated. Next is creation of JS list.

--- pieces.js
+++ pieces.js
@@ -1,30 +1,42 @@
-for (let i = 0; i < pieces.length; i++) {
-    
-    // Récupération des pièces depuis le fichier JSON
-    const reponse = await fetch("pieces-autos.json");
-    const pieces = await reponse.json();
+// Récupération des pièces depuis le fichier JSON
+const reponse = await fetch("pieces-autos.json");
+const pieces = await reponse.json();
 
+for (let i = 0; i < pieces.length; i++) {
     // Manipulation du DOM
-    const article = pieces[0];
+    const article = pieces[i];
+
+    // Récupération de l'élément du DOM qui accueillera les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d’une balise dédiée à une pièce automobile
+    const pieceElement = document.createElement("article");
+
     const imageElement = document.createElement("img");
-    imageElement.src = article.image;
+    imageElement.src = pieces[i].image;
+
     const nomElement = document.createElement("h2");
     nomElement.innerText = article.nom;
-    const categorieElement = document.createElement("p");
-    categorieElement.innerText = article.categorie;
+
     const prixElement = document.createElement("p");
     prixElement.innerText = `Prix: ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
 
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+
+    const descriptionElement = document.createElement("p"); 
+    descriptionElement.innerText = article.description ?? " ";
+
     // Si la machine n'apprtient à aucune catégorie
     const stockElement = document.createElement("p");
-    stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock"   
+    stockElement.innerText = article.disponibilite ? "[En stock]" : "[Rupture de stock]"   
 
     // Affiliation à un parent
-    const sectionFiches = document.querySelector(".fiches");
-    sectionFiches.appendChild(imageElement);
-    sectionFiches.appendChild(nomElement);
-    sectionFiches.appendChild(prixElement);
-    sectionFiches.appendChild(categorieElement);
-    document.body.appendChild(prixElement);
+    sectionFiches.appendChild(pieceElement);
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement)
 
 }
